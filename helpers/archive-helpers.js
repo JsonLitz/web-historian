9009a6ef509b0b867da188e03c7d3a6d9732d18a
Hack Reactor Student <student@hackreactor.com>
2017-07-26 19:24:07 -0700

    add archive helpers

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -26,16 +26,40 @@ exports.initialize = function(pathsObj) {
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(callback) {
+  fs.readFile(exports.paths.list, 'utf8', (err, success) => {
+    if (err){
+      console.log(err)
+    }else{
+      var temp = success.split('\n')
+      // console.log(temp);
+      callback(temp);
+    }
+  });
+  //this is the helper that will search through the queue of requested urls
+
 };
 
 exports.isUrlInList = function(url, callback) {
+  exports.readListOfUrls(function(data) {
+    callback(data.indexOf(url) > -1);
+  });
 };
 
 exports.addUrlToList = function(url, callback) {
+  //takes the input from the client and adds the URL to the queue
+  fs.writeFile(exports.paths.list, url, 'utf8',  callback);
+
 };
 
 exports.isUrlArchived = function(url, callback) {
+  fs.readdir(exports.paths.archivedSites, 'utf8', (err, files) => {
+    callback(files.indexOf(url) > -1);
+  })
+  //checks the archive for the existence of the requested url
 };
 
 exports.downloadUrls = function(urls) {
+  for (var i = 0; i < urls.length; i++) {
+    fs.writeFile(exports.paths.archivedSites + '/' + urls[i], urls[i]);
+  }
 };
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,15 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  fs.readFile(archive.paths.siteAssets + '/index.html', 'utf8', (err, success) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.end(success);
+    }
+  })
+  // res.end(archive.paths.siteAssets);
 };
